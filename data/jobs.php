8b0454bf656123f8d373e19fcc5994db8e670ff8
ardaaltinorsardaaltinors@gmail.com <ardaaltinors@gmail.com>
2024-05-25 15:31:09 +0300

    first commit

---
+++ data/jobs.php
@@ -0,0 +1,45 @@
+<?php
+$jobs = [
+    [
+        "title" => "Software Engineer",
+        "salary" => "$80,000",
+        "location" => "New York",
+        "type" => "Local",
+        "tags" => ["Development", "Coding"]
+    ],
+    [
+        "title" => "Marketing Specialist",
+        "salary" => "$70,000",
+        "location" => "San Francisco",
+        "type" => "Remote",
+        "tags" => ["Marketing", "Advertising"]
+    ],
+    [
+        "title" => "Web Developer",
+        "salary" => "$75,000",
+        "location" => "Los Angeles",
+        "type" => "Local",
+        "tags" => ["Web Development", "Programming"]
+    ],
+    [
+        "title" => "Data Analyst",
+        "salary" => "$65,000",
+        "location" => "Chicago",
+        "type" => "Remote",
+        "tags" => ["Data Analysis", "Statistics"]
+    ],
+    [
+        "title" => "Graphic Designer",
+        "salary" => "$60,000",
+        "location" => "Miami",
+        "type" => "Local",
+        "tags" => ["Graphic Design", "Creative"]
+    ],
+    [
+        "title" => "Data Scientist",
+        "salary" => "$90,000",
+        "location" => "Seattle",
+        "type" => "Remote",
+        "tags" => ["Data Science", "Machine Learning"]
+    ]
+];
---
+++ templates/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css" />
+    <title>CareerFinder</title>
+</head>
+
+<body class="bg-gray-100">
+    <!-- Nav -->
+    <header class="bg-blue-900 text-white p-4">
+        <div class="container mx-auto flex justify-between items-center">
+            <h1 class="text-3xl font-semibold">
+                <a href="index.php">CareerFinder</a>
+            </h1>
+            <nav class="space-x-4">
+                <a href="login.php" class="text-white hover:underline">Login</a>
+                <a href="register.php" class="text-white hover:underline">Register</a>
+                <a href="post-job.php" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
+                    <i class="fa fa-edit"></i> Post a Job
+                </a>
+            </nav>
+        </div>
+    </header>
